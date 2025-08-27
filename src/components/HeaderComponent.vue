acf61ad78a704859eac66823bf44a408b5bc5491
bernardomacieledu <bernardo.maciel.edu@gmail.com>
2025-08-26 22:24:10 -0300

    first commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,54 @@
+<script setup>
+import {RouterLink} from 'vue-router';
+</script>
+
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li class="atual">
+                    <router-link to="/">Home</router-link>
+                </li>
+                <li>
+                    <router-link to="/about">About</router-link>
+                </li>
+                <li>
+                    <router-link to="/projects">Projects</router-link>
+                </li>
+                <li>
+                    <router-link to="/connect">Connect</router-link>
+                </li>
+
+            </ul>
+        </nav>
+    </header>
+
+</template>
+
+<style scoped>
+header {
+    padding: 5vw 10vw;
+    font-size: 0.8rem;
+
+    & ul {
+        display: flex;
+        width: fit-content;
+        margin: 0 auto;
+        background: rgba(255, 255, 255, 0.5);
+        padding: 5px;
+        border-radius: 50px;
+        border: 1px solid rgb(211, 211, 211);
+
+        & li {
+            padding: 8px 15px;
+        }
+
+        & .router-link-active {
+            background: black;
+            color: white;
+            border-radius: 30px;
+            padding: 5px 10px;
+        }
+    }
+}
+</style>
